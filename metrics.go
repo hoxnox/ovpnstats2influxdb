c0ccbfc9c778355b212c0d2d4fc343ccc1c4ba99
steven leadbeater <stevenleadbeater@live.co.uk>
2021-04-26 16:24:58 +0100

    Output detailed metrics for clients

--- metrics.go
+++ metrics.go
@@ -13,3 +13,14 @@ type Metric struct {
 func createMetrics(clients []ovpnstats.ClientInfo, routes []ovpnstats.RoutingInfo) []Metric {
 	return []Metric{{map[string]interface{}{"clients": len(clients), "routes": len(routes)}, nil}}
 }
+
+func createClientMetrics(clients []ovpnstats.ClientInfo) []Metric {
+	var metrics []Metric
+	for _, client := range clients {
+		metrics = append(metrics, Metric{
+			map[string]interface{}{"sent": client.BytesSent, "received": client.BytesReceived},
+			map[string]string{"name": client.Name, "username": client.Username, "virtual_ip4": client.VirtualAddress},
+		})
+	}
+	return metrics
+}
--- telegraf.go
+++ telegraf.go
@@ -25,5 +25,17 @@ func RunTelegraf(path string) error {
 	for _, point := range points {
 		fmt.Println(point.String())
 	}
+
+	clientMetrics := createClientMetrics(clients)
+
+	clientPoints, err := createBatchPoints("openvpn", clientMetrics)
+	if err != nil {
+		return err
+	}
+
+	// output line protocol lines
+	for _, point := range clientPoints {
+		fmt.Println(point.String())
+	}
 	return nil
 }
